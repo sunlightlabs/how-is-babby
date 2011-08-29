638705e8768841de7b6b9e1b49ffa8f139259458
Alison Rowland <arowland@sunlightfoundation.com>
2011-08-29 15:30:44 -0400

    Hide the image before it gets an src attribute from the Javascript

--- webapp/viewer/static/js/viewer.js
+++ webapp/viewer/static/js/viewer.js
@@ -3,6 +3,8 @@ socket.on('connect', function () {
     socket.on('imgUrl', function (imgUrl) {
         var img = document.getElementById('video_frame');
         img.src = imgUrl;
+        // Image starts off hidden, to avoid a brief second of a broken image, so unhide it now.
+        img.style.display = '';
     });
 });
 
