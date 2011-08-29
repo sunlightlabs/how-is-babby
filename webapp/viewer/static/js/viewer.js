8961685ba44289a209bc5e944ac0a4d744a472b1
Aaron Bycoffe <bycoffe@gmail.com>
2011-08-29 13:59:17 -0400

    Change Node server

--- webapp/viewer/static/js/viewer.js
+++ webapp/viewer/static/js/viewer.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://localhost:8080');
+var socket = io.connect('http://ec2-50-17-169-160.compute-1.amazonaws.com:8080');
 socket.on('connect', function () {
     socket.on('imgUrl', function (imgUrl) {
         var div = document.getElementById('frames');
