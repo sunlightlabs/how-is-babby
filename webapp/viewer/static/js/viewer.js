8211b83f718bbe0e4e595d1dc312ff2c07391c4c
Aaron Bycoffe <bycoffe@gmail.com>
2011-08-29 14:04:17 -0400

    Add JavaScript to new index template

--- webapp/viewer/static/js/viewer.js
+++ webapp/viewer/static/js/viewer.js
@@ -2,7 +2,7 @@ var socket = io.connect('http://ec2-50-17-169-160.compute-1.amazonaws.com:8080')
 socket.on('connect', function () {
     socket.on('imgUrl', function (imgUrl) {
         var div = document.getElementById('frames');
-        div.innerHTML = '<img src="' + imgUrl + '"/>';
+        div.innerHTML = '<img width="300" height="300" src="' + imgUrl + '"/>';
     });
 });
 
