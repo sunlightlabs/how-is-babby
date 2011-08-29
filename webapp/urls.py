869b404ca8e8eb3ce1c422489b08b821d7f9bf89
Alison Rowland <arowland@sunlightfoundation.com>
2011-08-29 12:28:02 -0400

    Add basic template and root URL

--- webapp/urls.py
+++ webapp/urls.py
@@ -5,8 +5,7 @@ from django.conf.urls.defaults import *
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^webapp/', include('webapp.foo.urls')),
+    (r'^', 'viewer.views.index'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
