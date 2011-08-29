fa72ceb5ca620ede0c87dc16128a886fdbd04c36
Alison Rowland <arowland@sunlightfoundation.com>
2011-08-29 15:09:15 -0400

    Made JS replace img src attribute instead of entire innerHTML so styling would apply correctly

--- webapp/viewer/static/js/viewer.js
+++ webapp/viewer/static/js/viewer.js
@@ -1,8 +1,8 @@
 var socket = io.connect('http://ec2-50-17-169-160.compute-1.amazonaws.com:8080');
 socket.on('connect', function () {
     socket.on('imgUrl', function (imgUrl) {
-        var div = document.getElementById('frames');
-        div.innerHTML = '<img width="300" height="300" src="' + imgUrl + '"/>';
+        var img = document.getElementById('video_frame');
+        img.src = imgUrl;
     });
 });
 
