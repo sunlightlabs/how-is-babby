b7f2c1b3f67a0f1ad39757d57fd7e6518cdd6b00
Alison Rowland <arowland@sunlightfoundation.com>
2011-08-30 10:45:32 -0400

    Adding user profile to save config options

--- webapp/viewer/models.py
+++ webapp/viewer/models.py
@@ -1,5 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Alert(models.Model):
     event_type = models.CharField(max_length=6, null=False, blank=False)
+    # we'll also need a timestamp for queueing events
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+
+    motion_sensitivity = models.IntegerField()
+    #TODO:
+    #sound_sensitivity = models.IntegerField()
+
+    distance = models.IntegerField()
+
+    sms_email = models.EmailField()
+    sms_on = models.BooleanField()
+
+    nightvision_on = models.BooleanField()
+
+
+
 
