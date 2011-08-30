a6e60792fef01f6fec0da6594399760a22632c89
Alison Rowland <arowland@sunlightfoundation.com>
2011-08-30 14:03:22 -0400

    Got configuration options working

--- webapp/viewer/models.py
+++ webapp/viewer/models.py
@@ -15,21 +15,20 @@ MOTION_SENSITIVITY_CHOICES = ((5, 'Low'),(10, 'Medium'),(20, 'High'))
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
 
-    motion_sensitivity = models.IntegerField(choices=MOTION_SENSITIVITY_CHOICES, null=False, blank=False)
+    motion_sensitivity = models.IntegerField(choices=MOTION_SENSITIVITY_CHOICES, null=False, blank=False, default=10)
     #TODO:
     #sound_sensitivity = models.IntegerField()
 
-    distance = models.IntegerField()
+    distance = models.IntegerField(null=False, blank=False, default=10)
 
     sms_email = models.EmailField()
-    sms_on = models.BooleanField()
+    sms_on = models.BooleanField(null=False, default=False)
 
-    nightvision_on = models.BooleanField()
+    nightvision_on = models.BooleanField(null=False, default=False)
 
 
 class ConfigForm(ModelForm):
-    # TODO: this is not actually hiding the field yet...
-    user = forms.HiddenInput()
+    user = forms.IntegerField(widget=forms.HiddenInput())
     class Meta:
         model = UserProfile
 
