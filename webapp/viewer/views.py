7ffb2fcde6c46f284788fc22cdab86e2bb000a89
Alison Rowland <arowland@sunlightfoundation.com>
2011-08-30 23:59:52 -0400

    Get only recent (since last refresh) alerts, so as not to overwhelm the user/app

--- webapp/viewer/views.py
+++ webapp/viewer/views.py
@@ -2,10 +2,10 @@ from django.contrib.auth import authenticate, login
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.contrib import messages
-from viewer.models import Alert, ConfigForm, User
+from viewer.models import Alert, ConfigForm
 import persistent_messages
 #import time
-#from datetime import datetime, timedelta
+from datetime import datetime, timedelta
 
 def set_user(request):
     if not request.user.is_authenticated():
@@ -17,16 +17,17 @@ class FakeUser(object):
     def __init__(self):
         self.email = 'partytime@sunlightfoundation.com'
 
-def set_up_alert(request):
+def get_new_recent_alerts(request):
     user = authenticate(username='babby', password='onthemove')
     from_user = FakeUser()
-    for alert in Alert.objects.all():
-        persistent_messages.add_message(request, persistent_messages.WARNING, message='The babby is on the move!', subject='The babby is on the move!', extra_tags='warning', email=False, user=user, from_user=user, fail_silently=True)
-        alert.delete()
+
+    alerts = Alert.objects.filter(timestamp__gte=datetime.now()-timedelta(seconds=5)).order_by('timestamp')
+    if len(alerts):
+        persistent_messages.add_message(request, persistent_messages.WARNING, message='The babby is on the move!', subject='Alert', extra_tags='warning', email=False, user=user, from_user=user, fail_silently=True)
 
 
 def alerts_ajax(request):
-    set_up_alert(request)
+    get_new_recent_alerts(request)
 
     return render_to_response('messages.html', {}, context_instance=RequestContext(request))
 
@@ -44,7 +45,7 @@ def toggle_sms(request):
 
 def index(request):
     set_user(request)
-    set_up_alert(request)
+    get_new_recent_alerts(request)
     toggle_sms(request)
 
     return render_to_response('index.html',
@@ -53,7 +54,7 @@ def index(request):
 
 def log(request):
     set_user(request)
-    set_up_alert(request)
+    get_new_recent_alerts(request)
 
     return render_to_response('log.html',
                               {},
@@ -61,7 +62,7 @@ def log(request):
 
 def configure(request):
     set_user(request)
-    set_up_alert(request)
+    get_new_recent_alerts(request)
 
     profile = request.user.get_profile()
 
